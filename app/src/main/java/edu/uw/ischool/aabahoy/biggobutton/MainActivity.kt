729f44d1f8db8ce303a8ac98338bb4649b20236a
aabahoy-1677301 <81782054+aabahoy-1677301@users.noreply.github.com>
2023-10-24 23:02:05 -0700

    finished hw

--- app/src/main/java/edu/uw/ischool/aabahoy/biggobutton/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/aabahoy/biggobutton/MainActivity.kt
@@ -5,8 +5,19 @@ import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pushButton: Button = findViewById<Button>(R.id.button)
+        pushButton.setOnClickListener {
+            count += 1
+            if (count == 1) {
+                pushButton.text = getString(R.string.pushed_once)
+            } else {
+                pushButton.text = "You have pushed me $count times!"
+            }
+        }
     }
 }
